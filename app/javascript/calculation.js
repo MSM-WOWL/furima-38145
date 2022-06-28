0ffacbeecfbce69bb05c80b257c7b7f633351818
MSM-WOWL <decevollily@gmail.com>
2022-06-28 14:15:49 +0900

    Math.floorへの修正

--- app/javascript/calculation.js
+++ app/javascript/calculation.js
@@ -3,9 +3,9 @@ function calculation (){
   itemPrice.addEventListener("keyup", () => {
     const readVal = itemPrice.value;
     const addTaxPrice = document.getElementById("add-tax-price");
-    addTaxPrice.innerHTML = `${ Math.trunc(readVal * 0.1) }`;
+    addTaxPrice.innerHTML = `${ Math.floor(readVal * 0.1) }`;
     const profit = document.getElementById("profit");
-    profit.innerHTML = `${ Math.trunc(readVal * 0.9) }`;
+    profit.innerHTML = `${ Math.floor(readVal * 0.9) }`;
   });
 };
 
