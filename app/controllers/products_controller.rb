75f26028b7d782ae5ce4f6eb8fe3412c433681f2
MSM-WOWL <decevollily@gmail.com>
2022-06-29 14:10:29 +0900

    before_actionの追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_product, only: [:show, :edit, :update]
 
   def index
     @products = Product.order("created_at DESC")
@@ -19,18 +20,15 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def edit
-    @product = Product.find(params[:id])
     unless user_signed_in? && @product.user.id == current_user.id
       redirect_to action: :index
     end
   end
 
   def update
-    @product = Product.find(params[:id])
     if @product.update(product_params)
       redirect_to product_path
     else
@@ -39,7 +37,13 @@ class ProductsController < ApplicationController
   end
 
   private
+
   def product_params
     params.require(:product).permit(:item, :price, :explain, :category_id, :condition_id, :postage_id, :send_area_id, :send_day_id, :image).merge(user_id: current_user.id)
   end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+  
 end
